06c05f1bc112525276b499e208a1d293c1739537
Andrea Harris <Andrea@zacs-imac.lan>
2019-12-13 11:31:24 -0600

    working

--- starwars/src/components/StarwarsCard.js
+++ starwars/src/components/StarwarsCard.js
@@ -8,40 +8,36 @@ import starPeople from "./starPeople"
 
 
 export default function StarwarsCard(){
-    const [Starpeople, setStarPeople]= useState([{}]);
+    const [Starpeople, setStarPeople]= useState([]);
 }
 
 useEffect(()=>{
-    Axios.get('https://swapi.co/api/people/')
+    axios.get('https://swapi.co/api/people/')
     .then ((response)=>{
         console.log (response);
-        const People = response.data.results;
+         setStarPeople(response.data.results)
        
        }) 
        .catch (err =>{
         console.log(error)
-        
-        setStarPeople(People);
     })
-},[])
+},[]);
 
 return(
     <div>
-        {Starpeople.map(per =>{
-return
-       <starPeople
-       name={per.name}
-       birth={per.birth_year}
-       gender={per.gender}
-       world={per.homeworld}
-       films={per.films}
+        {Starpeople.map((per)=>{
+            return
 
-
-
-
-
-/>
+            <starPeople
+                name={per.name}
+                birth={per.birth_year}
+                gender={per.gender}
+                world={per.homeworld}
+                films={per.films}
+                        
+            
+            
+            />
         })}
-    </div>
+</div>
 )
-    
